03b92fd835c39ac293ba144883944599c4d6f4f8
brallan <brallan.rubio@correounivalle.edu.co>
2019-07-11 12:13:07 -0500

    virtualadress

---
+++ 11_07_2019/virtualadress.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	printf("localizacion del codigo : %p\n", main);
+	printf("localizacion del monton : %p\n", malloc(100e6));
+	int x = 3;
+	printf("localizacion de la pila : %p\n", &x);
+	return x;
+}
